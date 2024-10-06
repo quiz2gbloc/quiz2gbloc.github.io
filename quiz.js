34e3f07431c9aeaedae9bfa0691e14752c304b8c
quiz2gbloc <harrabi2021@gmail.com>
2024-10-06 11:51:33 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -14,6 +14,18 @@ const questions = [
     {
         question: "Le défibrillateur aide à:",
         answers: ["Restaurer la fonction cardiaque", "Anesthésier le patient", "Corriger l'hypotension"]
+    },
+    {
+        question: "Le système de stérilisation est dans:",
+        answers: ["Les salles d'opération", "L'urgence", "Les salles de radiologie", "La réception"]
+    },
+    {
+        question: "Le plateau universel est utilisé pour:",
+        answers: ["Organiser et présenter les instruments chirurgicaux", "Réaliser un certain type des chirurgies", "Assurer la sécurité du patient"]
+    },
+    {
+        question: "Le bistouri électrique est utilisé pour:",
+        answers: ["Effuctuer des opérations très précise", "Détruire et éliminer tous les micro-organismes", "Assurer la sécurié du patient"]
     }
 ];
 
@@ -67,7 +79,7 @@ function checkAnswers() {
         const userAnswer = form[`question${index}`].value;
         const correctAnswer = q.answers[0]; // Use the first answer as the correct one
         if (userAnswer === correctAnswer) {
-            score += 25; // Each correct answer adds 25 points
+            score += 10; // Each correct answer adds 10 points
         }
     });
 
@@ -76,13 +88,13 @@ function checkAnswers() {
     resultDiv.style.display = 'block'; // Show result div
 
     // Set the result message and score
-    if (score >= 75) {
-        resultMessage.textContent = "Congratulations, you passed!";
+    if (score >= 50) {
+        resultMessage.textContent = "Félicitations, vous avez réussi !";
     } else {
-        resultMessage.textContent = "Better luck next time!";
+        resultMessage.textContent = "Plus de chance la prochaine fois !";
     }
 
-    resultScore.textContent = `Your score is: ${score}/100`;
+    resultScore.textContent = `Votre score est: ${score}/70`;
 }
 
 // Load quiz on page load
